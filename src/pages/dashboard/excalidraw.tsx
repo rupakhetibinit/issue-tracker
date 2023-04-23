084718ea46dad185b130b68b06a7ae5a5fba893d
Binit Rupakheti <crusierponder@protonmail.com>
2023-04-23 14:52:44 +0545

    created dashboard with links

---
+++ src/pages/dashboard/excalidraw.tsx
@@ -0,0 +1,44 @@
+import { SidebarLinks, Sidebar } from '@/components/sidebar';
+import { HomeIcon, PenTool, LayoutGrid, ImageIcon } from 'lucide-react';
+import Head from 'next/head';
+
+function Excalidraw() {
+	return <div>Excalidraw</div>;
+}
+
+export default Excalidraw;
+
+Excalidraw.getLayout = function getLayout(page: JSX.Element) {
+	const sidebarLinks: SidebarLinks[] = [
+		{
+			name: 'Home',
+			path: '/dashboard',
+			icon: HomeIcon,
+		},
+		{ name: 'Projects', path: '/dashboard/projects', icon: PenTool },
+		{ name: 'Issues', path: '/dashboard/issues', icon: LayoutGrid },
+		{ name: 'Excalidraw', path: '/dashboard/excalidraw', icon: ImageIcon },
+		// {
+		// 	name: 'Issues',
+		// 	path: '/dashboard/issues',
+		// },
+	];
+	return (
+		<>
+			<Head>
+				<title>Dashboard | Issue Tracker</title>
+			</Head>
+
+			<main className='flex flex-col w-full h-screen px-4'>
+				<div className='w-2/12 py-4'>
+					<Sidebar links={sidebarLinks} />
+
+					{/* <Button variant='default' onClick={handleLogout}>
+					Logout
+				</Button> */}
+					{page}
+				</div>
+			</main>
+		</>
+	);
+};
