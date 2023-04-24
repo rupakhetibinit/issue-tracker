ae6d4f135f6aba72850a59fc5401aa67f3c4dc95
rupakhetibinit <crusierponder@protonmail.com>
2023-04-24 14:26:06 +0545

    added create project route in trpc

---
+++ src/server/routers/project.ts
@@ -0,0 +1,44 @@
+import { protectedProcedure, router } from '../trpc';
+import { z } from 'zod';
+
+export const projectRouter = router({
+	getAllProjects: protectedProcedure.query(async ({ ctx }) => {
+		const projects = await ctx.prisma.project.findMany({
+			where: {
+				members: {
+					some: {
+						userId: ctx.session.userId,
+					},
+				},
+			},
+		});
+
+		return projects;
+	}),
+	createProject: protectedProcedure
+		.input(
+			z.object({
+				title: z.string(),
+				description: z.string(),
+			})
+		)
+		.mutation(async ({ input, ctx }) => {
+			try {
+				const project = await ctx.prisma.project.create({
+					data: {
+						title: input.title,
+						description: input.description,
+					},
+				});
+				await ctx.prisma.userOnProject.create({
+					data: {
+						userId: ctx.session.userId,
+						projectId: project.id,
+					},
+				});
+				return project;
+			} catch (error) {
+				console.log(error);
+			}
+		}),
+});
