c369540911acc9cb8af1ea7d3485d772b9b865ee
rupakhetibinit <crusierponder@protonmail.com>
2023-04-25 09:14:06 +0545

    trpc update related to context and user validation

--- src/server/context.ts
+++ src/server/context.ts
@@ -2,29 +2,48 @@ import { auth } from '@/lib/lucia';
 import prisma from '@/lib/prisma';
 import type { inferAsyncReturnType } from '@trpc/server';
 import type { CreateNextContextOptions } from '@trpc/server/adapters/next';
+import { create } from 'domain';
 
 /**
  * Creates context for an incoming request
  * @link https://trpc.io/docs/context
  */
-export async function createContext(opts: CreateNextContextOptions) {
-	const authRequest = auth.handleRequest(opts.req, opts.res);
-	const session = await authRequest.validate();
-	const contextInner = await createContextInner({ session });
-	return {
-		...contextInner,
+export async function createTRPCContext(opts: CreateNextContextOptions) {
+	const { req, res } = opts;
+	//@ts-ignore
+	const authRequest = auth.handleRequest(req, res);
+	const session = await authRequest.validateUser();
+
+	const innerContext = await createInnerTRPCContext({
 		session,
+	});
+	return {
+		...innerContext,
+		req,
+		res,
 	};
 }
 interface CreateInnerContextOptions extends Partial<CreateNextContextOptions> {
-	session: Readonly<{
-		sessionId: string;
-		userId: string;
-		activePeriodExpiresAt: Date;
-		idlePeriodExpiresAt: Date;
-		state: 'idle' | 'active';
-		fresh: boolean;
-	}> | null;
+	session:
+		| {
+				user: null;
+				session: null;
+		  }
+		| {
+				user: {
+					userId: string;
+					username: string;
+					email: string;
+				};
+				session: Readonly<{
+					sessionId: string;
+					userId: string;
+					activePeriodExpiresAt: Date;
+					idlePeriodExpiresAt: Date;
+					state: 'idle' | 'active';
+					fresh: boolean;
+				}>;
+		  };
 }
 /**
  * Inner context. Will always be available in your procedures, in contrast to the outer context.
@@ -35,11 +54,11 @@ interface CreateInnerContextOptions extends Partial<CreateNextContextOptions> {
  *
  * @see https://trpc.io/docs/context#inner-and-outer-context
  */
-export async function createContextInner(opts?: CreateInnerContextOptions) {
+export async function createInnerTRPCContext(opts: CreateInnerContextOptions) {
 	return {
 		prisma,
-		session: opts?.session,
+		session: opts.session,
 	};
 }
 
-export type Context = inferAsyncReturnType<typeof createContext>;
+export type Context = inferAsyncReturnType<typeof createTRPCContext>;
