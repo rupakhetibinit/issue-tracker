ee14221cc04130bc8f96fd0ab180446f812a68f0
rupakhetibinit <crusierponder@protonmail.com>
2023-04-25 09:14:06 +0545

    trpc update related to context and user validation

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -1,10 +1,10 @@
 import * as trpcNext from '@trpc/server/adapters/next';
 import { appRouter } from '@/server/routers/_app';
-import { createContext } from '@/server/context';
+import { createTRPCContext } from '@/server/context';
 
 // export API handler
 // @see https://trpc.io/docs/api-handler
 export default trpcNext.createNextApiHandler({
 	router: appRouter,
-	createContext,
+	createContext: createTRPCContext,
 });
--- src/server/trpc.ts
+++ src/server/trpc.ts
@@ -1,20 +1,33 @@
 import { TRPCError, initTRPC } from '@trpc/server';
 import { Context } from './context';
+import { ZodError, transformer } from 'zod';
 // Avoid exporting the entire t-object
 // since it's not very descriptive.
 // For instance, the use of a t variable
 // is common in i18n libraries.
-const t = initTRPC.context<Context>().create();
+const t = initTRPC.context<Context>().create({
+	errorFormatter({ shape, error }) {
+		return {
+			...shape,
+			data: {
+				...shape.data,
+				zodError:
+					error.cause instanceof ZodError ? error.cause.flatten() : null,
+			},
+		};
+	},
+});
 
 const isAuthed = t.middleware(({ next, ctx }) => {
-	if (!ctx.session?.userId) {
+	if (!ctx.session?.user?.userId || !ctx.session.session) {
 		throw new TRPCError({
 			code: 'UNAUTHORIZED',
 		});
 	}
 	return next({
 		ctx: {
-			session: ctx.session,
+			session: ctx.session.session,
+			user: ctx.session.user,
 		},
 	});
 });
