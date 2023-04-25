0670ce3bbcc00219c1d7e7dbd5f678c6bda0e832
rupakhetibinit <crusierponder@protonmail.com>
2023-04-25 14:05:23 +0545

    fix redundancies

--- src/pages/dashboard/excalidraw.tsx
+++ src/pages/dashboard/excalidraw.tsx
@@ -1,44 +1,20 @@
-import { SidebarLinks, Sidebar } from '@/components/sidebar';
-import { HomeIcon, PenTool, LayoutGrid, ImageIcon } from 'lucide-react';
 import Head from 'next/head';
+import DashboardLayout from '@/components/dashboard-layout';
 
 function Excalidraw() {
-	return <div>Excalidraw</div>;
+	return <div className='mt-6 mx-2 w-full'>Excalidraw</div>;
 }
 
 export default Excalidraw;
 
 Excalidraw.getLayout = function getLayout(page: JSX.Element) {
-	const sidebarLinks: SidebarLinks[] = [
-		{
-			name: 'Home',
-			path: '/dashboard',
-			icon: HomeIcon,
-		},
-		{ name: 'Projects', path: '/dashboard/projects', icon: PenTool },
-		{ name: 'Issues', path: '/dashboard/issues', icon: LayoutGrid },
-		{ name: 'Excalidraw', path: '/dashboard/excalidraw', icon: ImageIcon },
-		// {
-		// 	name: 'Issues',
-		// 	path: '/dashboard/issues',
-		// },
-	];
 	return (
 		<>
 			<Head>
 				<title>Dashboard | Issue Tracker</title>
 			</Head>
 
-			<main className='flex flex-col w-full h-screen px-4'>
-				<div className='w-2/12 py-4'>
-					<Sidebar links={sidebarLinks} />
-
-					{/* <Button variant='default' onClick={handleLogout}>
-					Logout
-				</Button> */}
-					{page}
-				</div>
-			</main>
+			<DashboardLayout>{page}</DashboardLayout>
 		</>
 	);
 };
