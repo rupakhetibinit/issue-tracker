2ecbba16d0610f9ffde76bd37292cc32d72f0d6f
rupakhetibinit <crusierponder@protonmail.com>
2023-04-25 13:34:39 +0545

    add layout for dashboard

---
+++ src/components/dashboard-layout.tsx
@@ -0,0 +1,15 @@
+import { PropsWithChildren } from 'react';
+import { Sidebar } from './sidebar';
+import { Separator } from './ui/separator';
+
+export default function DashboardLayout(props: PropsWithChildren) {
+	return (
+		<main className='flex flex-row w-full h-screen px-4'>
+			<div className='w-2/12'>
+				<Sidebar />
+			</div>
+			<Separator orientation='vertical' className='m-1' />
+			{props.children}
+		</main>
+	);
+}
