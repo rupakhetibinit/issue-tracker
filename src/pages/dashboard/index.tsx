a216bf351c3773b7dbe12833542edd86d67ca4ad
rupakhetibinit <crusierponder@protonmail.com>
2023-04-25 17:39:47 +0545

    fix : remove unused imports, layout fix

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,7 +1,4 @@
-import { Sidebar, SidebarLinks } from '@/components/sidebar';
-import { Button } from '@/components/ui/button';
 import { auth } from '@/lib/lucia';
-import { trpc } from '@/utils/trpc';
 import { User } from 'lucia-auth';
 import {
 	GetServerSidePropsContext,
@@ -9,29 +6,27 @@ import {
 	InferGetServerSidePropsType,
 } from 'next';
 import Head from 'next/head';
-import Link from 'next/link';
-import { useRouter } from 'next/router';
 import {
-	HomeIcon,
-	ImageIcon,
-	LayoutGrid,
-	LucideIcon,
-	PenTool,
-} from 'lucide-react';
-import {
-	ReactElement,
-	JSXElementConstructor,
-	ReactFragment,
-	ReactPortal,
-	ReactNode,
-	PropsWithChildren,
-} from 'react';
+	Card,
+	CardHeader,
+	CardTitle,
+	CardDescription,
+	CardContent,
+	CardFooter,
+} from '@/components/ui/card';
+import UserActions from '@/components/user-actions';
+import DashboardLayout from '@/components/dashboard-layout';
 
-export const getServerSideProps = async (
-	context: GetServerSidePropsContext
-): Promise<GetServerSidePropsResult<{ user: User }>> => {
-	const authRequest = auth.handleRequest(context.req, context.res);
-	const { user } = await authRequest.validateUser();
+export const getServerSideProps = async ({
+	req,
+	res,
+}: GetServerSidePropsContext): Promise<
+	GetServerSidePropsResult<{ user: User }>
+> => {
+	//@ts-ignore
+	const session = auth.handleRequest(req, res);
+	const { user } = await session.validateUser();
+
 	if (!user)
 		return {
 			redirect: {
@@ -39,14 +34,7 @@ export const getServerSideProps = async (
 				permanent: false,
 			},
 		};
-	// if (!user.firstName && !user.lastName) {
-	// 	return {
-	// 		redirect: {
-	// 			destination: '/setup',
-	// 			permanent: false,
-	// 		},
-	// 	};
-	// }
+
 	return {
 		props: {
 			user,
@@ -57,45 +45,35 @@ export const getServerSideProps = async (
 function Dashboard(
 	props: InferGetServerSidePropsType<typeof getServerSideProps>
 ) {
-	const hello = trpc.hello.useQuery({ text: 'From TRPC' });
-	const proto = trpc.authed.useQuery();
-
-	return <></>;
+	return (
+		<div className='mt-6 mx-2 w-full'>
+			<UserActions username={props.user.username} />
+			<Card className='w-max px-4 h-max'>
+				<CardHeader>
+					<CardTitle>Projects</CardTitle>
+					<CardDescription>Card Description</CardDescription>
+				</CardHeader>
+				<CardContent>
+					<p>Card Content</p>
+				</CardContent>
+				<CardFooter>
+					<p>Card Footer</p>
+				</CardFooter>
+			</Card>
+		</div>
+	);
 }
 
 export default Dashboard;
 
 Dashboard.getLayout = function getLayout(page: JSX.Element) {
-	const sidebarLinks: SidebarLinks[] = [
-		{
-			name: 'Home',
-			path: '/dashboard',
-			icon: HomeIcon,
-		},
-		{ name: 'Projects', path: '/dashboard/projects', icon: PenTool },
-		{ name: 'Issues', path: '/dashboard/issues', icon: LayoutGrid },
-		{ name: 'Excalidraw', path: '/dashboard/excalidraw', icon: ImageIcon },
-		// {
-		// 	name: 'Issues',
-		// 	path: '/dashboard/issues',
-		// },
-	];
 	return (
 		<>
 			<Head>
 				<title>Dashboard | Issue Tracker</title>
 			</Head>
 
-			<main className='flex flex-row w-full h-screen px-4'>
-				<div className='w-2/12 py-4'>
-					<Sidebar links={sidebarLinks} />
-
-					{/* <Button variant='default' onClick={handleLogout}>
-					Logout
-				</Button> */}
-				</div>
-				{page}
-			</main>
+			<DashboardLayout>{page}</DashboardLayout>
 		</>
 	);
 };
