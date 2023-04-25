567f9907f8e8abbe5a7697a586449061e849711c
rupakhetibinit <crusierponder@protonmail.com>
2023-04-25 11:17:12 +0545

    fixed sidebar length to be constant

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -19,7 +19,7 @@ export function Sidebar({ className, links }: SidebarProps) {
 	const { route } = useRouter();
 
 	return (
-		<div className={cn('pb-12', className)}>
+		<div className={cn('pb-12 fixed max-w-max', className)}>
 			<div className='space-y-4 py-4'>
 				<div className='px-4 py-2'>
 					<h2 className='mb-2 px-2 text-lg font-semibold tracking-tight'>
@@ -34,7 +34,7 @@ export function Sidebar({ className, links }: SidebarProps) {
 										variant: route === link.path ? 'default' : 'ghost',
 										size: 'sm',
 									}),
-									'w-full justify-start'
+									'justify-start flex-row flex w-44'
 								)}
 								href={link.path}>
 								<link.icon className='mr-2 h-4 w-4' />
