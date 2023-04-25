0466517a2fc17b44c804426b305a59b17bb5a355
rupakhetibinit <crusierponder@protonmail.com>
2023-04-25 10:02:13 +0545

    fix prisma issue where it was called on client

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,16 +1,6 @@
 import { clsx, type ClassValue } from 'clsx';
 import { twMerge } from 'tailwind-merge';
-import { auth } from './lucia';
-import { IncomingMessage, OutgoingMessage } from 'http';
-import { User } from 'lucia-auth';
 
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
-}
-
-export async function getUser(req: IncomingMessage, res: OutgoingMessage) {
-	//@ts-ignore
-	const session = auth.handleRequest(req, res);
-	const { user } = await session.validateUser();
-	return user;
 }
