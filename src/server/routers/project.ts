59588195f58a8e33b5ab32d267d65beba7c75904
rupakhetibinit <crusierponder@protonmail.com>
2023-05-02 17:00:45 +0545

    add : trpc route for add/remove members in project

--- src/server/routers/project.ts
+++ src/server/routers/project.ts
@@ -1,4 +1,4 @@
-import { Roles } from '@prisma/client';
+import { Member, Roles } from '@prisma/client';
 import { protectedProcedure, router } from '../trpc';
 import { z } from 'zod';
 
@@ -83,4 +83,113 @@ export const projectRouter = router({
 				},
 			});
 		}),
+	getMembersOfProject: protectedProcedure
+		.input(z.object({ projectId: z.string() }).required())
+		.query(async ({ ctx, input }) => {
+			return await ctx.prisma.member.findMany({
+				where: {
+					projectId: input.projectId,
+				},
+				include: {
+					user: {
+						select: {
+							username: true,
+							firstName: true,
+							lastName: true,
+							id: true,
+						},
+					},
+				},
+			});
+		}),
+	createMemberInProject: protectedProcedure
+		.input(
+			z.object({
+				projectId: z.string(),
+				userList: z
+					.object({
+						firstName: z.string().nullable().optional(),
+						lastName: z.string().nullable().optional(),
+						username: z.string(),
+					})
+					.required()
+					.array(),
+			})
+		)
+		.mutation(async ({ ctx, input }) => {
+			let addedUsers: Member[] = [];
+			input.userList.map(async (user) => {
+				const addedUser = await ctx.prisma.member.create({
+					data: {
+						user: {
+							connect: {
+								username: user.username,
+							},
+						},
+						project: {
+							connect: {
+								id: input.projectId,
+							},
+						},
+						role: Roles.USER,
+					},
+				});
+				addedUsers.push(addedUser);
+			});
+			return addedUsers;
+		}),
+	deleteMemberInProject: protectedProcedure
+		.input(
+			z.object({ authUserId: z.string(), projectId: z.string() }).required()
+		)
+		.mutation(async ({ ctx, input }) => {
+			const admin = await ctx.prisma.member.findUnique({
+				where: {
+					projectId_authUserId: {
+						authUserId: ctx.session.userId,
+						projectId: input.projectId,
+					},
+				},
+			});
+			if (admin?.role === 'ADMIN' && admin?.projectId === input.projectId) {
+				const deleted = await ctx.prisma.member.delete({
+					where: {
+						projectId_authUserId: {
+							projectId: input.projectId,
+							authUserId: input.authUserId,
+						},
+					},
+				});
+			}
+		}),
+	getAllMembersExceptYourself: protectedProcedure
+		.input(z.object({ projectId: z.string() }).required())
+		.query(async ({ ctx, input }) => {
+			return await ctx.prisma.authUser.findMany({
+				where: {
+					NOT: {
+						OR: [
+							{
+								id: ctx.session.userId,
+							},
+							{
+								members: {
+									some: {
+										projectId: input.projectId,
+									},
+								},
+							},
+						],
+					},
+				},
+				select: {
+					username: true,
+					firstName: true,
+					lastName: true,
+				},
+			});
+		}),
+	// removeUser: protectedProcedure.input(z.object({username:z.string()}).required()).query(async ({ctx,input})=>{
+	//   return await ctx.prisma.
+	// })
 });
