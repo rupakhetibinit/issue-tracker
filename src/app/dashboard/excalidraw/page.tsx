e12e3ef30b17d8eb02b2a6cd1c20c0f4dc8f0ccc
rupakhetibinit <crusierponder@protonmail.com>
2023-05-05 13:11:55 +0545

    excalidraw: page level check if user is logged in

--- src/app/dashboard/excalidraw/page.tsx
+++ src/app/dashboard/excalidraw/page.tsx
@@ -1,11 +1,29 @@
+import prisma from '@/lib/prisma';
 import Excalidraw from './Excalidraw';
-
+import { redirect } from 'next/navigation';
+import { Session } from 'lucia-auth';
+import axios from 'axios';
 export const metadata = {
-	title: 'Where am I',
-	description: 'Testing more app directory',
+	title: 'Excalidraw | Issue Tracker',
+	description: 'Draw to your hearts content',
 };
 
-export default function Page() {
+async function getUser() {
+	const response = axios.get<Session>(
+		'http://localhost:3000/api/getusercookie',
+		{
+			withCredentials: true,
+		}
+	);
+	return response;
+}
+
+export default async function Page() {
+	const session = await getUser();
+	if (!session) {
+		return redirect('/signin');
+	}
+	console.log('excalidraw');
 	return (
 		<div className='mt-6 mx-2 w-full'>
 			<Excalidraw />
