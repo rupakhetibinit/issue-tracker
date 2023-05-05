abdd9cdec92d382088072d198e4ae9638c12c5f6
rupakhetibinit <crusierponder@protonmail.com>
2023-05-05 13:16:56 +0545

    auth: get cookie in server component

--- src/app/dashboard/excalidraw/page.tsx
+++ src/app/dashboard/excalidraw/page.tsx
@@ -1,6 +1,7 @@
 import prisma from '@/lib/prisma';
 import Excalidraw from './Excalidraw';
 import { redirect } from 'next/navigation';
+import { cookies } from 'next/headers';
 import { Session } from 'lucia-auth';
 import axios from 'axios';
 export const metadata = {
@@ -8,18 +9,10 @@ export const metadata = {
 	description: 'Draw to your hearts content',
 };
 
-async function getUser() {
-	const response = axios.get<Session>(
-		'http://localhost:3000/api/getusercookie',
-		{
-			withCredentials: true,
-		}
-	);
-	return response;
-}
-
 export default async function Page() {
-	const session = await getUser();
+	const cookieStore = cookies();
+	const session = cookieStore.get('auth_session');
+
 	if (!session) {
 		return redirect('/signin');
 	}
