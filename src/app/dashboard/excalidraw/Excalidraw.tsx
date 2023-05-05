c3c106b5078402cc1764b4463fcfea9369030409
rupakhetibinit <crusierponder@protonmail.com>
2023-05-05 12:27:30 +0545

    fix: excalidraw ssr issue

--- src/app/dashboard/excalidraw/Excalidraw.tsx
+++ src/app/dashboard/excalidraw/Excalidraw.tsx
@@ -1,7 +1,26 @@
 'use client';
-import { Excalidraw } from '@excalidraw/excalidraw';
-import type { ExcalidrawProps } from '@excalidraw/excalidraw/types/types';
+import type {
+	ExcalidrawAPIRefValue,
+	ExcalidrawProps,
+} from '@excalidraw/excalidraw/types/types';
+import {
+	ForwardRefExoticComponent,
+	MemoExoticComponent,
+	RefAttributes,
+	useEffect,
+	useState,
+} from 'react';
 
 export default function Excl(props: ExcalidrawProps) {
-	return <Excalidraw {...props} />;
+	const [Excalidraw, setExcalidraw] = useState<MemoExoticComponent<
+		ForwardRefExoticComponent<
+			ExcalidrawProps & RefAttributes<ExcalidrawAPIRefValue>
+		>
+	> | null>(null);
+	useEffect(() => {
+		import('@excalidraw/excalidraw').then((comp) =>
+			setExcalidraw(comp.Excalidraw)
+		);
+	});
+	return <>{Excalidraw && <Excalidraw {...props} />}</>;
 }
