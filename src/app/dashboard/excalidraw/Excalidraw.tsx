767a29726276ad30c2c063eddaf504256f97bf3b
rupakhetibinit <crusierponder@protonmail.com>
2023-05-09 14:19:08 +0545

    testing excalidraw apis.

--- src/app/dashboard/excalidraw/Excalidraw.tsx
+++ src/app/dashboard/excalidraw/Excalidraw.tsx
@@ -1,8 +1,10 @@
 'use client';
+import { MainMenu, exportToBlob } from '@excalidraw/excalidraw';
 import type {
 	ExcalidrawAPIRefValue,
 	ExcalidrawProps,
 } from '@excalidraw/excalidraw/types/types';
+import React from 'react';
 import {
 	ForwardRefExoticComponent,
 	MemoExoticComponent,
@@ -17,10 +19,56 @@ export default function Excl(props: ExcalidrawProps) {
 			ExcalidrawProps & RefAttributes<ExcalidrawAPIRefValue>
 		>
 	> | null>(null);
+	const [excAPI, setExcAPI] = useState<ExcalidrawAPIRefValue | null>(null);
+
 	useEffect(() => {
 		import('@excalidraw/excalidraw').then((comp) =>
 			setExcalidraw(comp.Excalidraw)
 		);
-	});
-	return <>{Excalidraw && <Excalidraw {...props} />}</>;
+	}),
+		[];
+	if (!Excalidraw) return <div>Loading Excalidraw. Please be patient...</div>;
+	return (
+		<>
+			{Excalidraw && (
+				<Excalidraw ref={(api) => setExcAPI(api)} {...props}>
+					{/* <MainMenu>
+						<MainMenu.Item
+							onSelect={async () => {
+								if (!excAPI?.ready) {
+									return;
+								}
+								const elements = excAPI.getSceneElements();
+								const appState = excAPI.getAppState();
+								const blob = await exportToBlob({
+									elements: elements,
+									appState: {
+										...appState,
+										exportWithDarkMode: false,
+									},
+									files: excAPI.getFiles(),
+									getDimensions: () => {
+										return {
+											width: window.outerWidth,
+											height: window.outerHeight,
+										};
+									},
+								});
+								console.log(blob);
+
+								const formData = new FormData();
+								formData.append('image', blob);
+								await fetch('http://localhost:3000/api/save-image', {
+									method: 'POST',
+									body: formData,
+									credentials: 'include',
+								});
+							}}>
+							Export
+						</MainMenu.Item>
+					</MainMenu> */}
+				</Excalidraw>
+			)}
+		</>
+	);
 }
